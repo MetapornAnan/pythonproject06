c60a36f517d076ec97c8ae3714abb8807b2ff8b0
MetapornAnan <s6519410054@sau.ac.th>
2023-09-22 11:26:12 +0700

    code v.2 : if if-else else if ohohoh

--- test01.py
+++ test01.py
@@ -1,4 +1,5 @@
-#Control Flow
+#Control Flow / control Statement
+
 #Condition / Selection
 #กระบวนการทำการต้องมีการพิสูจน์/ตรวจสอบ ก่อนที่จะทำงานใดๆ
 # คำสั่ง if, if-else, if-else-if
@@ -7,3 +8,6 @@
 # Loop / Repeat / Iteration
 # กระบวนการทำการที่มีการทำงานเดิมๆ ซ้ำ ซ้ำกี่รอบก็ได้แต่ต้องรู้จบ
 # คำสั่ง while, for
+
+# break
+# continue
---
+++ test05.py
@@ -0,0 +1,10 @@
+# while หลัง while คือเงื่อนไข เป็นจริงทำไปเรื่อยๆ
+
+x = 1
+
+while x <= 10 :
+    print('IoT')
+    x = x + 2
+
+print(f'X => {x}')
+print('SAU')
