8119758457a185432549fc00fa3fc4493e5f8097
MetapornAnan <s6519410054@sau.ac.th>
2023-09-22 09:10:46 +0700

    code v.1 : create test01 jaaa

---
+++ test01.py
@@ -0,0 +1,9 @@
+#Control Flow
+#Condition / Selection
+#กระบวนการทำการต้องมีการพิสูจน์/ตรวจสอบ ก่อนที่จะทำงานใดๆ
+# คำสั่ง if, if-else, if-else-if
+
+
+# Loop / Repeat / Iteration
+# กระบวนการทำการที่มีการทำงานเดิมๆ ซ้ำ ซ้ำกี่รอบก็ได้แต่ต้องรู้จบ
+# คำสั่ง while, for
